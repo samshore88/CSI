9789db30704f3eadaae7f42eaab33667241dbe87
samsh <s1shore@student.bridgew.edu>
2026-09-15 15:54:45 -0400

    init

---
+++ 9/15 Lab.py
@@ -0,0 +1,20 @@
+# Part 1:
+name : "Sam Shore"
+major = "Cyber Security"
+town = "Bridgewater"
+age = 18
+GPA = 3.4
+
+# Part 2:
+fallcredits = 100
+springcredits = 100
+
+# Part 3:
+TUITION_PER_CREDIT = 100
+SCHOLARSHIP = 1,000
+DREAM_TUITION = 12,500.000
+
+# Part 4:
+college_name = "Bridgewater State University"
+print ("My dream college is in Bridgewater State University")
+
