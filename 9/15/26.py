97a75deb06425ecc4d1f247bb70953c6b99a1043
samsh <s1shore@student.bridgew.edu>
2026-09-15 15:10:00 -0400

    init

---
+++ 9/15/26.py
@@ -0,0 +1,28 @@
+# Question 1:
+movie = "Terminator 2"
+genre = "Action/Sci-fi"
+year = 1991
+rating = 9.5/10
+actor = "Arnold Schwarzenegger"
+character = "T-800"
+
+print ("My Movie")
+print ("--------")
+print ("Movie: Terminator 2")
+print ("Genre: Action/Sci-fi")
+print ("Release Year: 1991")
+print ("Rating: 9.5/10")
+print ("Actor: Arnold Schwarzenegger")
+print ("Character: T-800")
+
+# Question 2:
+people = 31
+groups = people // 3
+print (groups)
+leftover = people % 3
+print (leftover)
+
+#Question 3:
+fahrenheit = 77
+celsius = 25
+conversion = C=(F-32)x5/9
