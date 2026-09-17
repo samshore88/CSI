a638f8527379ac31a6cb39bdcc89bc82c6886474
samsh <s1shore@student.bridgew.edu>
2026-09-17 15:04:52 -0400

    init

---
+++ 9 17 26.py
@@ -0,0 +1,27 @@
+# Question 3:
+from shutil import SameFileError
+
+cupsofcoffee = 6
+costofcup = 10
+print(f"{costofcup * cupsofcoffee}")
+# Question 4:
+name = "James"
+print(f"Hello, {name}. Do you want to play a game?")
+# Question 5:
+uppercase = "Sam"
+lowercase = "sam"
+titlecase = "SAM"
+print(f"{uppercase}")
+print(f"{lowercase}")
+print(f"{titlecase}")
+# Question 6:
+name = "The Joker"
+quote = "Why so serious?"
+print(f"{name}once said {quote}')
+# Question 7:
+famous_person = "The Joker"
+quote = "Why so serious?"
+print(f"{famous_person}once said {quote}')
+# Question 8:
+name = Sam
+print("name")
